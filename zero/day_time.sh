59dfda3cdd93b9652fd4342b5293a7d76cf83da3
Bruce Thomas <response.write@gmail.com>
2019-08-12 17:05:27 +0100

    update daytime

--- zero/day_time.sh
+++ zero/day_time.sh
@@ -1,21 +1,60 @@
 #!/bin/bash
 
-#url="https://api.sunrise-sunset.org/json?lat=51.5465589&lng=-0.0352543&date=today"
-#wget $url -O - > day_time.json
-
-# Extract the civil_twilight_(begin|end) attributes
-json=`cat day_time.json | sed -r 's/.*("civ[^,]+").*("civ[^,]+").*/\1;\2/g'`
 
 #
+#
+#   ██████   █████  ██    ██ ████████ ██ ███    ███ ███████ 
+#   ██   ██ ██   ██  ██  ██     ██    ██ ████  ████ ██      
+#   ██   ██ ███████   ████      ██    ██ ██ ████ ██ █████ 
+#   ██   ██ ██   ██    ██       ██    ██ ██  ██  ██ ██    
+#   ██████  ██   ██    ██       ██    ██ ██      ██ ███████ 
+#                                                           
+#
+#
+#   Save JSON outputted from a URL
+#   https://stackoverflow.com/questions/3040904/save-json-outputted-from-a-url-to-a-file
+
+
+
+# Incoming argument (optional) "true" to trigger API call
+update=$1
+
+# Go to google maps to find your lat/lng geolocation
+lat=51.5465589
+lng=-0.0352543
+
+apiurl="https://api.sunrise-sunset.org/json?lat=$lat&lng=$lng&date=today"
+today="./sunrise-sunset.json"
+
+# time offset to before/after the sunrise/sunset event
+offset="90 minutes"
+now=`date +"%H%M%S"`
+
+# The API call only needs to update once per day
+# the $update value is an argument sent by ./main.sh
+# if the JSON does not exist then call the API
+if [[ $update == "true" ||  ! -f $today ]]; then
+    wget $apiurl -O - > $today
+fi
+
+# Extract the civil_twilight_(begin|end) attributes
+json=`cat $today | sed -r 's/.*("civ[^,]+").*("civ[^,]+").*/\1;\2/g'`
+
 # extract the respective time values
 read sunrise <<< $(echo $json | awk -F";" '{print $1}' | sed -r 's/.*"([^\s]+)".*/\1/g')
 read sunset <<< $(echo $json | awk -F";" '{print $2}' | sed -r 's/.*"([^\s]+)".*/\1/g')
 
-# time offset to before/after the sunrise/sunset event
-offset="90 minutes"
 
 # Convert the AM/PM time to 24hour time and extract the time value as long (eg. 0345512)
 sunrise=$(date -d "$sunrise-$offset" | awk '{print $4}' | sed -r s/\://g)
 sunset=$(date -d "$sunset+$offset" | awk '{print $4}' | sed -r s/\://g)
 
-echo "sunrise" $sunrise "sunset" $sunset
+# determine if currently between sunrise/sunset
+sunset="170000"
+if [ $now -gt $sunrise -a $now -lt $sunset ]; then
+    daytime=1
+else
+    daytime=0
+fi
+
+echo "sunrise" $sunrise "sunset" $sunset "now" $now "daytime" $daytime
--- zero/main.sh
+++ zero/main.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
 
+#    
+#     ██████████   ██████   ██ ███████
+#    ░░██░░██░░██ ░░░░░░██ ░██░░██░░░██
+#     ░██ ░██ ░██  ███████ ░██ ░██  ░██
+#     ░██ ░██ ░██ ██░░░░██ ░██ ░██  ░██
+#     ███ ░██ ░██░░████████░██ ███  ░██
+#    ░░░  ░░  ░░  ░░░░░░░░ ░░ ░░░   ░░
+#
+#   
+#   1.  Runs and infinte loop that takes a photo
+#   2.  Creates the daily folder destination
+#   3.  Once a day grabs the sunrise-sunset.json
+#
+#
+#
+#
+
+
+
+
 # ffmpeg -f concat -safe 0 -i <(find images/ -regex ".*jpg$" -printf "file `pwd`/%p\n" | sort -V) -vf scale=640x480 text.mkv
 
 # One photo every 30 minutes
@@ -14,8 +34,12 @@ hour=$(date +"%H")
 late="23"
 early="03"
 
+awake=$(echo `sh ./day_time.sh` | awk '{print $8}')
+echo "AWAKE: $awake"
 
-while true; do
+
+while true
+do
 
     if [ "$hour" -lt "$late" ] && [ "$hour" -gt "$early" ]; then
         sh ./single.sh "$z"
