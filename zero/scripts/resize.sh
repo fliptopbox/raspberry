344f47e686b9d8a1a7304fc8c0ef2efd2f72a6ff
Bruce Thomas <response.write@gmail.com>
2019-08-18 17:55:27 +0100

    Moved the delete action

--- zero/scripts/resize.sh
+++ zero/scripts/resize.sh
@@ -9,11 +9,15 @@ Q=`dirname "${this}"`
 # and put that in ../images/current/
 # and delete the previous file(s)
 
+mkdir -p $relativeCurrent
 
 filename=$(echo $1 | awk -F'/' '{print $NF}')
 resizedImage="$relativeCurrent/$filename"
 
-rm $relativeCurrent/*
 convert $1 -resize x1080 $resizedImage
+
+# Delete older files AFTER we have a replacement
+count=`ls $relativeCurrent/*.jpg | wc -l`
+rm `ls -tr $relativeCurrent/*.jpg | head -$(( $count-1 ))` 
 
 echo ">> $resizedImage"
