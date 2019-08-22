db33c76b0e35297958466c0811e1a8cbc4b88ebb
Bruce Thomas <response.write@gmail.com>
2019-08-22 12:39:19 +0100

    remove py http.server from boot

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # The bootloader will run theses scripts
-# Don't forget the trailing &
+# Don't forget the trailing & 
+# or add them to a detached screen session
 
 
 cd /home/pi/Projects/github/raspberry/zero/
 cd scripts/
 screen -dmS main ./main.sh
 
-cd ../http/
-screen -dmS http python3 -m http.server
+# http is now handled by apache2
+# cd ../http/
+# screen -dmS http python3 -m http.server
