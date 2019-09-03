8d1a6605c541665eaef5ac8a0c6190e7037762f4
Bruce Thomas <response.write@gmail.com>
2019-09-03 16:30:43 +0100

    added FE continuous setting

--- zero/http/js/usage.js
+++ zero/http/js/usage.js
@@ -1,15 +1,23 @@
 export default usage;
 function usage({ disk }) {
-    const percent = disk.percent;
-    const fith = (percent / 20) >> 0;
-    // const colors = ['green', 'green', 'orange', 'red', 'black'];
-    const colors = ['white', 'white', 'white', 'white', 'red'];
-    const color = colors[fith];
+    const {percent, avail} = disk;
+    const GiB = avail / (1000 ** 2);
+    const color = percent > 75 ? "red" : "white";
+    const deg = (360 * (percent / 100) ) >> 0;
+
+    console.log(percent, deg);
 
     const html = `
         <div class="widget">
-            <span class="icon usage" 
-                style="--bgcolor:${color}; --width:${(fith + 1) * 20}%">
+            <span class="icon disk"></span>
+            <span class="text">${GiB >> 0}GB</span>
+        </div>
+        <div class="widget">
+            <span class="icon pie" 
+                style="
+                    --pie-color:${color};
+                    --pie-percent:${deg}deg
+                ">
             </span>
             <span class="text">${percent}%</span>
         </div>
